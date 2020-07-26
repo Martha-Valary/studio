42e93965c1e25320790222e572fb103e3c20176b
Martha-Valary <marthavallary@gmail.com>
2020-07-27 00:01:29 +0300

    add some elements to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -49,3 +49,49 @@ $(document).ready(function(){
       $("#overlay3").hide();
     });
   });
+  $(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+    $("#work6").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+    $("#work7").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+    $("#work8").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+      event.preventDefault();
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  });
